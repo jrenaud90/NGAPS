3c0fcd7bc68b6931ef97197595f3e1a9372ad243
Jrenaud-Desk <joe.p.renaud@gmail.com>
2020-05-08 14:09:30 -0400

    got rid of html loader - was not importing in right order

--- NGAPS_NEW/js/nav.js
+++ NGAPS_NEW/js/nav.js
@@ -1,8 +1,10 @@
-function navFunction() {
+function navCollapseFunction() {
     var x = document.getElementById("myTopnav");
+
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
         x.className = "topnav";
     }
+
 }
